0f64870afb32b0b7b3ff2b62f8876abc69f4c701
ikbalhussa1n <121198342+ikbalhussa1n@users.noreply.github.com>
2024-09-19 07:20:27 -0400

    Initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+function toggleMenu() {
+    const menu = document.querySelector(".menu_links");
+    const icon = document.querySelector(".ham_nav_lines");
+    menu.classList.toggle("open")
+    icon.classList.toggle("open")
+}
+
+
